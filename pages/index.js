77a32cf737735732adb5eb90b2397232464a7848
omer-akkoca <omerakkoca1042@gmail.com>
2023-04-17 18:13:04 +0300

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,52 @@
+import { AnimatedText, Layout, LinkArrow } from "@/src/components";
+import Image from "next/image";
+import profilePic from "../public/images/developer-pic-1.png"
+import Link from "next/link";
+
+const cv = "https://firebasestorage.googleapis.com/v0/b/omer-akkoca.appspot.com/o/omer_akkoca-cv-en.pdf?alt=media"
+
 const Home = () => {
-    return(
-        <div>
-            <h1 className='text-red-700 text-4xl font-mont'>PORTFOLIO</h1>
+    return (
+        <div className="flex items-center text-dark w-full min-h-screen">
+            <Layout className="pt-0">
+                <div className="flex items-center justify-between w-full">
+                    <div className="w-1/2">
+                        <Image
+                            src={profilePic} alt="profile-photo"
+                            className="w-full h-auto"
+                        />
+                    </div>
+                    <div className="w-1/2 flex flex-col items-center self-center">
+                        <AnimatedText
+                            text={"Turning Vision Into Reality With Code And Design"}
+                            className="!text-6xl !text-left"
+                        />
+                        <p className="my-4 text-base font-medium">
+                            As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
+                            Explore my latest projects and articles, showcasing my expertise in React.js and web development.
+                        </p>
+                        <div className="flex items-center self-start mt-2">
+                            <Link
+                                href={cv} target="_blank"
+                                className="
+                                flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
+                                border-2 border-solid border-transparent hover:border-dark
+                                "
+                                download={true}
+                            >
+                                Resume
+                                <LinkArrow className={"w-6 ml-1"}/>
+                            </Link>
+                            <Link
+                                href={"mailto:omerakkoca11042@gmail.com"} target="_blank"
+                                className="ml-4 text-lg font-medium capitalize text-dark underline"
+                            >
+                                Contact
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </Layout>
         </div>
     )
 }
